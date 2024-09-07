ab18f35bd36ebf1c73176600b1fd222a112c4308
Matheus Dalla Costa <matheusdc1997@gmail.com>
2024-09-07 19:51:43 -0300

    home page prototipo

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:pedra_papel_tesoura/pages/home.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Pedra, papel e tesoura",
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
+  ));
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Pedra, papel e tesoura"),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Sua jogada!",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Row(
+              children: [
+                Image.asset("images/pedra.png", height: 100),
+                Image.asset("images/papel.png", height: 100),
+                Image.asset("images/tesoura.png", height: 100),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Jogada do computador!",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Image.asset("images/default.png", height: 130),
+          ],
+        ),
+      ),
+    );
+  }
+}
